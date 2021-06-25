332270f847368bc6d0e76288fb83d6adfcb49eb5
Leminhtam-419 <tamle135>
2021-06-25 15:47:37 +0700

    get data

---
+++ lib/resapi_one/screens/Home.dart
@@ -0,0 +1,42 @@
+import 'package:appmau/resapi_one/model/model.dart';
+import 'package:appmau/resapi_one/services/service.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  Future<Model> futureAlbum;
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FutureBuilder<Model>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.name);
+            } else if (snapshot.hasError) {
+              print('ggggg${snapshot.error}');
+              return Text("${snapshot.error}");
+            }
+
+            // By default, show a loading spinner.
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/resapi_one/services/service.dart
+++ lib/resapi_one/services/service.dart
@@ -1,13 +1,16 @@
+import 'dart:convert';
+
 import 'package:appmau/resapi_one/model/model.dart';
 import 'package:http/http.dart' as http;
 Future<Model> fetchAlbum() async {
   final response =
-  await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  await http.get(Uri.parse('http://makeup-api.herokuapp.com/api/v1/products.json/'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Model.fromJson(jsonDecode(response.body));
+    final jsonresponse = json.decode(response.body);
+    return Model.fromJson(jsonresponse);
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
